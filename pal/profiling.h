1f58061c71e99fa9358bc0e1a16511ffc9c3fc28
Justin King-Lacroix <justin.kinglacroix@gmail.com>
2012-04-04 14:21:47 +0100

    profiling data structures are now common

--- pal/profiling.h
+++ pal/profiling.h
@@ -1,21 +1,7 @@
 #ifndef __PROFILING_H__
 #define __PROFILING_H__
 
-#include <stdint.h>
-
-enum {
-	PROFILE_NULL = 0,
-	PROFILE_ENTER,
-	PROFILE_EXIT,
-	PROFILE_INSIDE,
-};
-
-typedef struct {
-	const char* function;
-	const char* description;
-	unsigned long type;
-	uint32_t timestamp;
-} timing_point_t;
+#include <bottlecap/profiling.h>
 
 #ifdef BOTTLE_CAP_PROFILE
 
