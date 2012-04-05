dec3c6ba393b0e27c7a6cdd8342d2f6bc1025711
Justin King-Lacroix <justin.kinglacroix@gmail.com>
2012-04-05 16:11:09 +0100

    added module for isolating a bottle from a result

---
+++ client/bottleonly.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include <bottlecap/bottlecalls.h>
+#include <bottlecap/errors.h>
+
+#include "params.h"
+
+int do_real_work(void) {
+	/* Allocate and encrypt a cap for future tests.
+	 */
+
+	//now generate the approprate stuff
+	uint32_t call = 0;
+
+	//call number
+	int pm_append(int paramType, char *paramData, int paramSize);
+	if(pm_append(BOTTLE_CALL, (char*)&call, sizeof(call)) != sizeof(call)) {
+		printf("unable to append call number\n");
+		return -ENOMEM;
+	}
+
+	//bring in bottle header and copy
+	char* data_in;
+	int size;
+	if((size = pm_get_addr(BOTTLE_HEADER, &data_in)) <= 0) {
+		printf("BOTTLECAP: Could not get header\n");
+		return -EINVAL;
+	}
+	if(pm_append(BOTTLE_HEADER, data_in, size) != size) {
+		printf("unable to append header\n");
+		return -ENOMEM;
+	}
+
+	//bring in bottle table and copy
+	if((size = pm_get_addr(BOTTLE_TABLE, &data_in)) <= 0) {
+		printf("BOTTLECAP: Could not get table\n");
+		return -EINVAL;
+	}
+	if(pm_append(BOTTLE_TABLE, data_in, size) != size) {
+		printf("unable to append table\n");
+		return -ENOMEM;
+	}
+
+	return 0;
+}
+
