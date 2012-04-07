800556ad5dcb7bf9610da0b5f4bb96c219d74a6a
Justin King-Lacroix <justin.kinglacroix@gmail.com>
2012-04-07 14:33:32 +0100

    suite now checks for existence

--- pal/suite.sh
+++ pal/suite.sh
@@ -1,12 +1,14 @@
 REPCOUNT=100
 
 for i in ../io/*.in; do
-	SUITE=`basename $i`
-	echo "suite $SUITE start"
-	cp $i flicker.in
-	./bench.sh $REPCOUNT
-	mkdir $SUITE
-	mv `seq $REPCOUNT |xargs` $SUITE
-	mv $SUITE $i.bench
-	echo "suite $SUITE done"
+	if [ ! -d $i.bench ]; then
+		SUITE=`basename $i`
+		echo "suite $SUITE start"
+		cp $i flicker.in
+		./bench.sh $REPCOUNT
+		mkdir $SUITE
+		mv `seq $REPCOUNT |xargs` $SUITE
+		mv $SUITE $i.bench
+		echo "suite $SUITE done"
+	fi
 done
